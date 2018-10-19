585e22990f1793d7b8dd9c0426a8d1e29431971c
yexuesong <yexuesong218@gmail.com>
2018-10-19 18:24:27 +0800

    add annotation

--- www/coroweb.py
+++ www/coroweb.py
@@ -13,8 +13,9 @@ from apis import APIError
 
 
 def get(path):
-    """
-    Define decorator @get('/path')
+    """Define decorator @get('/path')
+
+    @get装饰器，给处理函数绑定URL和HTTP method-GET的属性
     """
 
     def decorator(func):
@@ -30,9 +31,7 @@ def get(path):
 
 
 def post(path):
-    """
-    Define decorator @post('/path')
-    """
+    """Define decorator @post('/path')"""
 
     def decorator(func):
         @functools.wraps(func)
@@ -47,6 +46,7 @@ def post(path):
 
 
 def get_required_kw_args(fn):
+    """将函数所有 没默认值的 命名关键字参数名 作为一个tuple返回"""
     args = []
     params = inspect.signature(fn).parameters
     for name, param in params.items():
@@ -56,6 +56,7 @@ def get_required_kw_args(fn):
 
 
 def get_named_kw_args(fn):
+    """将函数所有的 命名关键字参数名 作为一个tuple返回"""
     args = []
     params = inspect.signature(fn).parameters
     for name, param in params.items():
@@ -65,6 +66,7 @@ def get_named_kw_args(fn):
 
 
 def has_named_kw_args(fn):
+    """检查函数是否有命名关键字参数"""
     params = inspect.signature(fn).parameters
     for name, param in params.items():
         if param.kind == inspect.Parameter.KEYWORD_ONLY:
@@ -72,6 +74,7 @@ def has_named_kw_args(fn):
 
 
 def has_var_kw_arg(fn):
+    """检查函数是否有关键字参数集"""
     params = inspect.signature(fn).parameters
     for name, param in params.items():
         if param.kind == inspect.Parameter.VAR_KEYWORD:
@@ -79,14 +82,14 @@ def has_var_kw_arg(fn):
 
 
 def has_request_arg(fn):
-    sig = inspect.signature(fn)
-    params = sig.parameters
+    """检查函数是否有request参数，返回布尔值。若有request参数，检查该参数是否为该函数的最后一个参数，否则抛出异常。"""
+    params = inspect.signature(fn).parameters
     found = False
     for name, param in params.items():
         if name == 'request':
             found = True
             continue
-        if found and (
+        if found and (  # 如果找到'request'参数后，还出现位置参数，就会抛出异常
                 param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
             raise ValueError(
                 'request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
@@ -94,10 +97,11 @@ def has_request_arg(fn):
 
 
 class RequestHandler(object):
+    """请求处理器，用来封装处理函数"""
 
     def __init__(self, app, fn):
-        self._app = app
-        self._func = fn
+        self._app = app  # app: an application instance for registering the fn
+        self._func = fn  # fn: a request handler with a particular HTTP method and path
         self._has_request_arg = has_request_arg(fn)
         self._has_var_kw_arg = has_var_kw_arg(fn)
         self._has_named_kw_args = has_named_kw_args(fn)
@@ -105,27 +109,37 @@ class RequestHandler(object):
         self._required_kw_args = get_required_kw_args(fn)
 
     async def __call__(self, request):
+        """分析请求
+
+        A request handler can be any callable that accepts a Request instance as its only argument and returns a StreamResponse derived (e.g. Response) instance.
+        A handler may also be a coroutine, in which case aiohttp.web will await the handler.
+        """
         kw = None
-        if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
+        if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:  # 当传入的处理函数具有 关键字参数集 或 命名关键字参数 或 request参数
             if request.method == 'POST':
-                if not request.content_type:
+                if not request.content_type:  # 无正文类型信息时返回
                     return web.HTTPBadRequest('Missing Content-Type.')
                 ct = request.content_type.lower()
-                if ct.startswith('application/json'):
+                if ct.startswith('application/json'):  # 处理JSON类型的数据，传入参数字典中
                     params = await request.json()
                     if not isinstance(params, dict):
                         return web.HTTPBadRequest('JSON body must be object.')
                     kw = params
-                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
+                elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith(
+                        'multipart/form-data'):  # 处理表单类型的数据，传入参数字典中
                     params = await request.post()
                     kw = dict(**params)
                 else:
                     return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
             if request.method == 'GET':
                 qs = request.query_string
-                if qs:
-                    kw = dict()
+                if qs:  # 获取URL中的请求参数，如 name=James, id=007
+                    kw = dict()  # 将请求参数传入参数字典中
                     for k, v in parse.parse_qs(qs, True).items():
+                        """
+                        parse a query string, data are returned as a dict. the dict keys are the unique query variable names and the values are lists of values for each name
+						a True value indicates that blanks should be retained as blank strings
+                        """
                         kw[k] = v[0]
         if kw is None:
             kw = dict(**request.match_info)
@@ -180,7 +194,7 @@ def add_routes(app, module_name):
     if n == (-1):
         mod = __import__(module_name, globals(), locals())
     else:
-        name = module_name[n+1:]
+        name = module_name[n + 1:]
         mod = getattr(__import__(module_name[:n], globals(), locals(), [name]), name)
     for attr in dir(mod):
         if attr.startswith('_'):
